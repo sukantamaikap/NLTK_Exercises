bdb7fa0e97ff79251c00d71d44310975333c0ef7
sukantamaikap <sukanta.maikap@gmail.com>
2017-10-02 09:02:29 +0530

    lemmatizing

--- NLP_Processes.py
+++ NLP_Processes.py
@@ -1,6 +1,7 @@
 import nltk
 from nltk.tokenize import PunktSentenceTokenizer
 from nltk.corpus import state_union
+from nltk.stem import WordNetLemmatizer
 
 content = state_union.raw('2006-GWBush.txt')
 tokenizer = PunktSentenceTokenizer()
@@ -20,9 +21,20 @@ def process_content():
             # }<put chinking content inside this>{ and {<chunking content inside this>}
             chunk_parser = nltk.RegexpParser(chunk_gram)
             chunked = chunk_parser.parse(tagged)
-
             print(chunked)
-            chunked.draw()
+            #chunked.draw()
+
+
+            # find named entity
+            named_entity = nltk.ne_chunk(tagged, binary=True)
+            #named_entity.draw()
+
+            #word lemmatizing
+            lemmatizer = WordNetLemmatizer()
+            print(lemmatizer.lemmatize('cats'))
+            print(lemmatizer.lemmatize('algae'))
+            print(lemmatizer.lemmatize('better', pos='a'))
+
 
     except Exception as e:
         print(str(e))
